eb55114317ecba0bccbc80914393953bb8b4e6cc
diegorezm <dmourarezende@email.com>
2025-06-01 14:17:39 -0300

    adding bulk nodes and transactions endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 
 	bl "github.com/diegorezm/DBlockchain/internals/blockchain"
@@ -9,18 +11,32 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	blockchain := bl.NewBlockchain()
 
-	blockchainHandlers(mux)
+	port := flag.Int("port", 4000, "Port to listen on")
+	flag.Parse()
 
-	addr := ":4000"
-	fmt.Printf("Server listening on http://localhost%s/\n", addr)
-	http.ListenAndServe(addr, mux)
+	blockchainHandlers(mux, blockchain)
+
+	addr := fmt.Sprintf(":%d", *port)
+	fullAddr := fmt.Sprintf("http://127.0.0.1%s", addr)
+	blockchain.AppendNode(fullAddr)
+
+	fmt.Printf("Server listening on port %s\n", addr)
+	log.Fatalf("%v", http.ListenAndServe(addr, mux))
 }
 
-func blockchainHandlers(mux *http.ServeMux) {
-	blockchain := bl.NewBlockchain()
+func blockchainHandlers(mux *http.ServeMux, blockchain *bl.Blockchain) {
 	blockchainHandler := bl.NewHandler(blockchain)
 
-	mux.Handle("GET /get_chain", http.HandlerFunc(blockchainHandler.GetChain))
-	mux.Handle("POST /mine", http.HandlerFunc(blockchainHandler.Mine))
+	mux.Handle("GET /chain", http.HandlerFunc(blockchainHandler.GetChain))
+	mux.Handle("GET /chain/is_valid", http.HandlerFunc(blockchainHandler.IsValid))
+	mux.Handle("POST /chain/mine", http.HandlerFunc(blockchainHandler.Mine))
+	mux.Handle("POST /chain/replace", http.HandlerFunc(blockchainHandler.ReplaceChain))
+
+	mux.Handle("POST /transactions/add", http.HandlerFunc(blockchainHandler.AddTransaction))
+	mux.Handle("POST /transactions/add/bulk", http.HandlerFunc(blockchainHandler.AddTransactionBulk))
+
+	mux.Handle("POST /nodes/add", http.HandlerFunc(blockchainHandler.AddNode))
+	mux.Handle("POST /nodes/add/bulk", http.HandlerFunc(blockchainHandler.AddNodeBulk))
 }
